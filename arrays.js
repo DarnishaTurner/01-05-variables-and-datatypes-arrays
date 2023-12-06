d399fb6838ba4ebcc9a9c19a0618bceb86b1e877
DarnishaTurner <darnishaturner@pursuit.org>
2023-12-05 21:08:02 -0500

    Darnishas Pushed

---
+++ arrays.js
@@ -0,0 +1,12 @@
+// 1. Create an array 'fruits' with the names of three different fruits.
+let fruits = ['Blue Raspberry', 'Strawberry', 'Lemon'];
+
+// 2. Add two more fruits to the 'fruits' array.
+fruits.push('Grapes', 'Pineapple');
+
+// 3. Create an array 'numbers' and fill it with five different numbers.
+let numbers = [1, 3, 5, 7, 9];
+
+// 4. Print the first and last item of the 'numbers' array.
+console.log('First item in numbers array:', numbers[0]);
+console.log('Last item in numbers array:', numbers[numbers.length - 1]);
